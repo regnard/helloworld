980ee5b157aa4ced3092266092c444a52adc4f8c
Regnard Raquedan <regnard@raquedan.com>
2021-08-27 01:16:59 -0400

    Add files via upload

---
+++ index.php
@@ -0,0 +1,21 @@
+<!doctype html>
+
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <title>Hello, World!</title>
+  <meta name="description" content="Hello, World!">
+  <meta name="author" content="Regnard Raquedan">
+  <meta property="og:title" content="Hello, World!">
+  <meta property="og:type" content="website">
+  <meta property="og:description" content="Hello, World!">
+</head>
+<body>
+<h1>Hello, World!</h1>
+<p>
+  <?php
+    echo gethostname();
+    ?></p>
+</body>
+</html>
